5174b47994e151ac6cbd478477038ffa0150d94a
Felipe <fvhmanhaes@gmail.com>
2024-06-20 23:23:14 -0300

    Add create routine

--- src/classes/IntrumentoCrud.js
+++ src/classes/IntrumentoCrud.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
+const Instrumento = require('./Instrumento');
+const crypto = require('crypto');
+
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = crypto.randomBytes(1)[0] % 16;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 
 class InstrumentoCrud {
     #path
@@ -11,8 +22,24 @@ class InstrumentoCrud {
 
 
     iCreate(obj){
+      try {
+        const data = fs.readFileSync('./files/instrumentos.json', 'utf-8');
+        let parsedData = JSON.parse(data);
+        const UUID = generateUUID()
+        const newInstrument = new Instrumento(UUID,obj.nome,obj.tipo,obj.estado)       
 
+        parsedData = [...parsedData,newInstrument]
+        fs.writeFileSync('./files/instrumentos.json', JSON.stringify(parsedData, null, 2), 'utf-8');
+
+        console.log('Object added successfully!');
+
+      } catch (err) {
+        console.error('Error reading or parsing the file:', err);
+      }
     }
+
+
+    
     iRead(){
         try {
             const data = fs.readFileSync('./files/instrumentos.json', 'utf-8');
--- src/instrumentos.js
+++ src/instrumentos.js
@@ -1,39 +1,64 @@
-const readline = require('readline/promises');
-const Instrumento = require('./classes/Instrumento')
-const InstrumentoCrud = require('./classes/IntrumentoCrud')
+const readline = require("readline/promises");
+const Instrumento = require("./classes/Instrumento");
+const InstrumentoCrud = require("./classes/IntrumentoCrud");
+
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
+function validate(input) {
+  const regex = /^([^,]*),([^,]*),([^,]*)$/;
+  const match = input.match(regex);
+
+  if (match) {
+    return match.slice(1);
+  } else {
+    return null;
+  }
+}
+
 async function run() {
-    const crud = new InstrumentoCrud
+  const crud = new InstrumentoCrud();
 
-    let resposta = await rl.question('Escolha uma ação (criar, deletar, alterar, consultar): ');
-    resposta = 'consultar'
-    switch (resposta) {
-        case 'criar':
-         
-            rl.close();
-            break;
-        case 'deletar': {
-            /* Coloque sua resposta aqui */
-            rl.close();
-            break;
-        }
-        case 'consultar': {
-            const resposta = crud.iRead()
-            console.log('resposta é:' )
-            console.log(resposta)
-            rl.close();
-            break;
-        }
-        default:
-            console.log("Ação não reconhecida.");
-            rl.close();
+  let resposta = await rl.question(
+    "Escolha uma ação (criar, deletar, alterar, consultar): "
+  );
+  resposta = "criar";
+  switch (resposta) {
+    case "criar":
+      let respostaCriar = await rl.question(
+        "Digite as informações do instrumento, separando por virgulas. (nome,tipo,estado): "
+      );
+      const validInput = validate(respostaCriar);
+       
+      validInput
+        ? crud.iCreate({
+    
+            nome: validInput[0],
+            tipo: validInput[1],
+            estado: validInput[2],
+          })
+        : console.log("\nThe input Instrument is not valid. Try again.\n");
+      rl.close();
+      break;
+    case "deletar": {
+      /* Coloque sua resposta aqui */
+      rl.close();
+      break;
     }
-
+    case "consultar": {
+      const resposta = crud.iRead();
+      console.log("resposta é:");
+      console.log(resposta);
+      rl.close();
+      break;
+    }
+    default:
+      console.log("Ação não reconhecida.");
+      rl.close();
+  }
 }
 
 run();
